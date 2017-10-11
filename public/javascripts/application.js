38a8d051aedb52453449ba7fb3fd90f3270ff5bd
Matt Fielding <matt.fielding@codeenigma.com>
2017-10-11 17:56:18 +0100

    utilise phone camera

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -103,6 +103,24 @@ function showHideRadioToggledContent() {
 
 }
 
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+      $('#blah').attr('src', e.target.result);
+    }
+
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+
+$("#id-document").change(function(){
+  readURL(this);
+  $('#blah').style.display = 'block';
+});
+
+
 $(document).ready(function() {
 
   showHideCheckboxToggledContent();
